030d29adc24a7b2c1383ddd03de172752e39027e
vtorm-b <vtorm-b@mail.ru>
2021-04-29 21:31:27 +0800

    1.4. Вызов метода с возвращаемым типом. [#464751]

--- src/main/java/ru/job4j/tracker/oop/DummyDic.java
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -2,12 +2,12 @@ package ru.job4j.tracker.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-//       String rsl = "Неизвестное слово - " + eng;
-//        System.out.println(rsl);
+        String rsl = "Неизвестное слово - " + eng;
+        System.out.println(rsl);
         return "Неизвестное слово - " + eng;
     }
     public static void main(String[] args) {
       DummyDic result = new DummyDic();
-      result.engToRus("word");
+      result.engToRus();
     }
 }
