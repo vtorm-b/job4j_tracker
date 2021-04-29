08bfdbff0f729e776b8d6ea18cedd2c1de15e59c
vtorm-b <vtorm-b@mail.ru>
2021-04-29 09:55:50 +0800

    1.8. Состояние объекта [#464752]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int load;
+    public Battery(int size){
+        this.load = size;
+    }
+
+    public void exchange (Battery another){
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery one = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("Старт " + one.load + " Космос " + second.load);
+        one.exchange(second);
+        System.out.println("Старт " + one.load + " Космос " + second.load);
+    }
+}
