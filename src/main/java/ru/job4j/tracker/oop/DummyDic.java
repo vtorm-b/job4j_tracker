9e81aa74f2a8cd152f07110dad6908c576764ece
vtorm-b <vtorm-b@mail.ru>
2021-04-29 22:39:28 +0800

    1.8. Состояние объекта [#464752]

--- src/main/java/ru/job4j/tracker/oop/DummyDic.java
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -2,11 +2,10 @@ package ru.job4j.tracker.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        return eng;
+        return "Неизвестное слово - " + eng;
     }
     public static void main(String[] args) {
       DummyDic result = new DummyDic();
-      result.engToRus("word");
-        System.out.println("Неизвестное слово - " + result);
+        System.out.println(result.engToRus("word"));
     }
 }
--- src/main/java/ru/job4j/tracker/oop/Point.java
+++ src/main/java/ru/job4j/tracker/oop/Point.java
@@ -4,7 +4,20 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
-    public double distance(int x1, int y1, int x2, int y2) {
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    private int x;
+    private int y;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point (0, 4);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
