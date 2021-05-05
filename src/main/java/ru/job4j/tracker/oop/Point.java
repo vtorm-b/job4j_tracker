fb0ec2ccd516f91d286a14f26849719a67592b3f
vtorm-b <vtorm-b@mail.ru>
2021-05-05 13:02:54 +0800

    2.2. Рефакторинг - Площадь треугольника. [#464743]

--- src/main/java/ru/job4j/tracker/oop/Point.java
+++ src/main/java/ru/job4j/tracker/oop/Point.java
@@ -16,7 +16,7 @@ public class Point {
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
-        Point b = new Point (0, 4);
+        Point b = new Point (0, 2);
         double dist = a.distance(b);
         System.out.println(dist);
     }
