8b6d969298b5424d77063876f4abe3a91054f291
vtorm-b <vtorm-b@mail.ru>
2021-05-11 19:11:15 +0800

    2. Перегрузка конструктора [#464758]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,10 +3,28 @@ package ru.job4j.tracker;
 public class Item {
     private int id;
     private String name;
+    private int price;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Код товара " + id);
+        System.out.println("Название товара " + name);
+        System.out.println("Цена товара " + price);
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+
+    }
 
     public int getId() {
         return id;
     }
+
 
     public void setId(int id) {
         this.id = id;
@@ -20,3 +38,5 @@ public class Item {
         this.name = name;
     }
 }
+
+
