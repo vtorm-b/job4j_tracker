28fbfd176f2e4529f2a4e7f3bba32b3e6d9a9a0e
vtorm-b <vtorm-b@mail.ru>
2021-05-21 13:09:51 +0800

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setStudent("Sergey Gerasimov");
+        student.setGroup("development");
+        student.setCreated(new Date());
+        System.out.println("студент: " + student.getStudent() + ", " + "группа: " + student.getGroup() + ", " + "дата: "
+                + student.getCreated());
+
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/pojo/Police.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.pojo;
+
+import java.util.Date;
+
+public class Police {
+    public static void main(String[] args) {
+        License license = new License();
+        license.setOwner("Sergey Gerasimov");
+        license.setModel("Toyota");
+        license.setCode("x111xx");
+        license.setCreated(new Date());
+        System.out.println(license.getOwner() + " -has a car-" + license.getModel() + " : " + license.getCode());
+    }
+}
