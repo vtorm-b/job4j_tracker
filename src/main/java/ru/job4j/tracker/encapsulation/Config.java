0c9cd0ca166761ab55047e107a5ff1d5da8ee6c8
vtorm-b <vtorm-b@mail.ru>
2021-05-21 16:16:19 +0800

    0. Что такое инкапсуляция. [#3818]

--- src/main/java/ru/job4j/tracker/encapsulation/Config.java
+++ src/main/java/ru/job4j/tracker/encapsulation/Config.java
@@ -1,14 +1,14 @@
 package ru.job4j.tracker.encapsulation;
 
 public class Config {
-    String name;
+    private String name;
     private int position;
     private String[] properties;
 
     public Config(String name) {
         this.name = name;
     }
-    void print() {
+    public void print() {
         System.out.println(position);
     }
     public String getProperties(String key) {
