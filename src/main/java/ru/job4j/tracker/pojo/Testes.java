a1e290853d7aed7d43ba561935a0da364ce60222
vtorm-b <vtorm-b@mail.ru>
2021-05-31 16:13:36 +0800

    4. Сравнение моделей. Метод equals [#174893]

--- src/main/java/ru/job4j/tracker/pojo/Testes.java
+++ src/main/java/ru/job4j/tracker/pojo/Testes.java
@@ -2,21 +2,9 @@ package ru.job4j.tracker.pojo;
 
 public class Testes {
     public static void main(String[] args) {
-        Product[] products = new Product[3];
-        products[0] = new Product("milk", 1);
-        products[1] = new Product("Bread", 2);
-        products[2] = new Product("-",1);
-        for (int i = products.length -1; i > -1; i--) {
-            Product pr = products[i];
-            System.out.println(pr);
-    }
-        products[1] = null;
-        for (int i = 0; i < products.length; i++) {
-            System.out.println(i);
+            Product first = new Product("Milk", 100);
+            Product second = new Product("Milk", 100);
+            boolean eq = first.equals(second);
+            System.out.println(eq);
         }
-        Product first = new Product("Milk", 100);
-        Product second = new Product("Milk", 100);
-        boolean eq = first.equals(second);
-        System.out.println(eq);
-}
-}
+    }
