0763e7f6d5d3ab2d90b1f2236ebee465f413b283
vtorm-b <vtorm-b@mail.ru>
2021-05-31 17:34:54 +0800

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/tracker/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/tracker/pojo/ShopDrop.java
@@ -3,9 +3,6 @@ package ru.job4j.tracker.pojo;
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
-            products[i] = products[i];
-        }
-            for (int i = index; i < products.length - 1; i++) {
                 products[i] = products[i + 1];
             }
         products[products.length - 1] = null;
