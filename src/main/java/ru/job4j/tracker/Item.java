eb91479e32c70148f2fccd1cfe16222adaf84b96
vtorm-b <vtorm-b@mail.ru>
2021-06-03 11:20:05 +0800

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -38,7 +38,6 @@ public class Item {
         return id;
     }
 
-
     public void setId(int id) {
         this.id = id;
     }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,6 +25,34 @@ public class Tracker {
         }
         return rsl;
     }
+    public Item[] findAll() {
+        Item[] tmp = new Item[items.length];
+        for (int i = 0, j = 0 ; i < tmp.length; i++) {
+                if (this.items[i] != null) {
+                    tmp[j++] = items[i];
+                }
+        }
+            return tmp;
+    }
+    public Item findByName(String key) {
+        Item[] result = new Item[items.length];
+        for (int i = 0, j = 0; i < items.length; i++) {
+            if(items[i] != null && items[i].getName().equals(key)) {
+                result[j++] = items[i];
+            }
+        }
+        return result[result.length];
+    }
+
+    public Item findById(int id) {
+        Item result = null;
+        for (Item item: items) {
+            if (item != null && item.getId().equals(id)){
+            result = item;
+            break;
+        }
+    }
+        return result;
 }
 
 
