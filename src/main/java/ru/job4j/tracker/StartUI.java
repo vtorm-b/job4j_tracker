ddfed1a5a4130d59c8d9f708a5b55b9aa81f8d5c
vtorm-b <vtorm-b@mail.ru>
2021-06-07 12:08:06 +0800

    5. Tracker - хранилище [#396 #163257]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,18 +1,23 @@
-/*package ru.job4j.tracker;
+package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+       /* Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String result = item.getCreated().format(formatter);
         System.out.println(result);
+        */
+        Tracker tracker = new Tracker();
+
     }
+
+
 }
 
- */
+
 
 
 
