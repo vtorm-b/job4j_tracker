652071b1d6ae651c3b84977cff3ccd799570c0ff
vtorm-b <vtorm-b@mail.ru>
2022-04-22 11:37:23 +0800

    2. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,23 +1,22 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
-    public static void main(String[] args) {
-       /* Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String result = item.getCreated().format(formatter);
-        System.out.println(result);
-        */
-        Tracker tracker = new Tracker();
-        tracker.add(new Item("test1"));
-        Item item = tracker.findById(1);
-        System.out.println(item);
+
+    public void init(Scanner scanner, Tracker tracker) {
 
     }
 
+    private void showMenu() {
 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
 }
 
 
--- src/main/java/ru/job4j/tracker/io/Matches.java
+++ src/main/java/ru/job4j/tracker/io/Matches.java
@@ -12,11 +12,15 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " Введите число от 1 до 3 :");
             int matches = Integer.parseInt(input.nextLine());
-            turn = !turn;
-            count = count - matches;
-            if (matches > 3) {
+          turn = !turn;
+          count = count - matches;
+            if (matches < 1 || matches > 3 || matches > count ) {
                 System.out.println("Ввели неверное число");
                 System.out.println("Остаток: " + count);
+            }
+            else {
+                turn = !turn;
+                count = count - matches;
             }
             if (!turn) {
                 System.out.println("Выиграл первый игрок!");
