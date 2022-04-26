e94a5dd9c69f5d0fb84351adbd2df283f5b1bb42
vtorm-b <vtorm-b@mail.ru>
2022-04-26 14:30:40 +0800

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,27 +5,40 @@ import java.util.Scanner;
 public class StartUI {
 
     public void init(Scanner scanner, Tracker tracker) {
-    boolean run  = true;
-    while (run) {
-        showMenu();
-        System.out.println("Select: ");
-        int select = Integer.parseInt(scanner.nextLine());
-        if (select == 0) {
-            System.out.println("==Create add new Item==");
-            System.out.print("Enter name: ");
-            String name = scanner.nextLine();
-            Item item = new Item(name);
-            tracker.add(item);
-            System.out.println("Добавлена заявка: " + item);
-        } else if (select == 6){
-            run = false;
-            System.out.println("Пользователь выбрал:" + select);
-        } else {
-            run = false;
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("==Create add new Item==");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавлена заявка: " + item);
+            } else if (select == 6) {
+                run = false;
+                System.out.println("Пользователь выбрал:" + select);
+            } else {
+                run = false;
+            }
+            if (select == 1) {
+                System.out.println("== Show all items==");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище не содержит заявок");
+                }
+            }
         }
     }
 
-    }
+
+
 
     private void showMenu() {
         String[] menu  = { "Add new item", "Show all items", "Edit item",
