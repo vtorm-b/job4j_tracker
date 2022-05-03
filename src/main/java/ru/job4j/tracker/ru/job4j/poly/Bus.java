0dffbd9d4268c7577bd733012b7839b3a3d424c3
vtorm-b <vtorm-b@mail.ru>
2022-05-03 15:34:44 +0800

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.ru.job4j.poly;
+
+
+public class Bus implements Transport{
+    @Override
+    public String drive(String drive) {
+        return null;
+    }
+
+    @Override
+    public int passenger(String passenger) {
+        return 0;
+    }
+
+    @Override
+    public int fuel(String price) {
+        return 0;
+    }
+}
