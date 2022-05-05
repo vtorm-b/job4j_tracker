67a2bf0a8af2c80e5e2ae3474f039205f5593ddd
vtorm-b <vtorm-b@mail.ru>
2022-05-05 18:12:33 +0800

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #292184]

--- src/main/java/ru/job4j/tracker/pojo/Product.java
+++ src/main/java/ru/job4j/tracker/pojo/Product.java
@@ -17,23 +17,29 @@ public class Product {
         Product product = (Product) o;
         return count == product.count && Objects.equals(name, product.name);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, count);
     }
+
     public Product(String name, int count) {
         this.name = name;
         this.count = count;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getCount() {
         return count;
     }
+
     public void setCount(int count) {
         this.count = count;
     }
