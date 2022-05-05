d183e852fc6ba8a41dd66640eac0f81d4dc41f6f
vtorm-b <vtorm-b@mail.ru>
2022-05-05 18:00:51 +0800

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #292184]

--- src/main/java/ru/job4j/tracker/pojo/Shop.java
+++ src/main/java/ru/job4j/tracker/pojo/Shop.java
@@ -3,12 +3,13 @@ package ru.job4j.tracker.pojo;
 public class Shop {
     public static int indexOfNull(Product[]products) {
         for (int i = 0; i < products.length; i++) {
-            if(products[i] == null) {
+           if(products[i] == null) {
                 return i;
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
         Product[] products = new Product[5];
         products[0] = new Product("milk",10);
