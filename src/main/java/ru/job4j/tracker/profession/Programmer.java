647cd9994c9933e21dea682492a1e3b2154ec149
vtorm-b <vtorm-b@mail.ru>
2022-05-05 17:11:11 +0800

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #292184]

--- src/main/java/ru/job4j/tracker/profession/Programmer.java
+++ src/main/java/ru/job4j/tracker/profession/Programmer.java
@@ -3,8 +3,8 @@ package ru.job4j.tracker.profession;
 public class Programmer extends Engineer {
 
     public Programmer(String name, String surname, String education, int birthday,
-                      String profession, String specialization,
-                      String development, String tester) {
+                      String profession, String specialization
+                      /*String development, String tester*/) {
         super(name, surname, education, birthday, profession, specialization);
     }
 }
