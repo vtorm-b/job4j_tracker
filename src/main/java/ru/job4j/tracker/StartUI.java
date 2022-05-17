8f24136bfa8c652405af19d4ef3eda25826751ec
vtorm-b <vtorm-b@mail.ru>
2022-05-17 19:26:43 +0800

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -32,7 +32,6 @@ public class StartUI {
             int select = input.askInt("Выберите пункт меню:");
             if (select == 0) {
                 StartUI.createItem(input, tracker);
-
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -40,7 +39,6 @@ public class StartUI {
                     for (Item item : items) {
                         System.out.println(item);
                     }
-
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
