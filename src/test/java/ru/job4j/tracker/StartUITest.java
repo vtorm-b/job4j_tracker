23ae3425930ef3efa90207d7705de8feedfc243d
vtorm-b <vtorm-b@mail.ru>
2022-06-20 14:10:01 +0800

    5. Input и полиморфизм. [#181094]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,7 +11,7 @@ public class StartUITest {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input,tracker);
+        StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(),is(expected.getName()));
